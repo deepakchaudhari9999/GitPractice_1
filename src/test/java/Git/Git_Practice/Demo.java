76da7523c3a7368aec4ee085527cb004b384f711
deepakchaudhari9999 <deepakchaudhari9999@gmail.com>
2024-03-13 10:39:02 +0530

    first round of commit...

---
+++ src/test/java/Git/Git_Practice/Demo.java
@@ -0,0 +1,28 @@
+package Git.Git_Practice;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Demo {
+
+	WebDriver driver;
+
+	@Test
+	public void mOne() {
+
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.get("https://web.whatsapp.com/");
+
+		driver.manage().window().maximize();
+
+	}
+
+}
